f052e40358ce6d14471fd4ac2770a740f0f24940
broma0 <121064585+broma0@users.noreply.github.com>
2023-04-27 19:01:54 +0000

    add el function to window

--- src/santoku/web/window.cpp
+++ src/santoku/web/window.cpp
@@ -8,14 +8,27 @@ extern "C" {
 
 using namespace emscripten;
 
-int alert (lua_State *L) {
+int l_alert (lua_State *L) {
   val win = val::global("window");
-  win.call<val>("alert", val(luaL_checkstring(L, -1)));
+  val query = val(luaL_checkstring(L, -1));
+  win.call<val>("alert", query);
+  return 1;
+}
+
+int l_el (lua_State *L) {
+  val win = val::global("window");
+  val query = val(luaL_checkstring(L, -1));
+  val nodes = win.call<val>("querySelectorAll", query);
+  lua_newtable(L);
+  lua_pushstring(L, "nodes");
+  lua_pushlightuserdata(L, nodes);
+  lua_settable(L, -2);
   return 1;
 }
 
 static luaL_Reg const fns[] = {
-  { "alert", alert },
+  { "alert", l_alert },
+  { "el", l_el },
   { NULL, NULL }
 };
 
