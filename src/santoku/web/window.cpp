326626818798a5a755e0223d08b4d50f36adf099
broma0 <121064585+broma0@users.noreply.github.com>
2023-04-27 19:26:08 +0000

    fix lua stack index in window module

--- src/santoku/web/window.cpp
+++ src/santoku/web/window.cpp
@@ -22,7 +22,7 @@ int l_el (lua_State *L) {
   lua_pushstring(L, "nodes");
   val *nodesp = (val *)lua_newuserdatauv(L, sizeof(val), 0);
   *nodesp = doc.call<val>("querySelectorAll", query);
-  lua_settable(L, -2);
+  lua_settable(L, -3);
   return 1;
 }
 
