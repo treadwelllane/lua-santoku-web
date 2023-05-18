465ce4862d30f9f77b266ae0183509f3f2e26d16
broma0 <121064585+broma0@users.noreply.github.com>
2023-05-18 17:45:14 +0000

    update val module and comprehensive tests

--- src/santoku/web/val.cpp
+++ src/santoku/web/val.cpp
@@ -15,102 +15,162 @@ extern "C" {
 using namespace std;
 using namespace emscripten;
 
-#define MT "santoku_web_val"
+// TODO: Add checkudata calls that use these
+#define MT "santoku_web"
+#define MTV "santoku_web_val"
+
 #define debug(...) printf("%s:%d\t", __FILE__, __LINE__); printf(__VA_ARGS__); printf("\n");
 
+int l_from (lua_State *);
+
 val *peek_val (lua_State *L, int i) {
-  val **vp = (val **)lua_touserdata(L, i);
-  return *vp;
+  val **vp = (val **)luaL_checkudata(L, i, MTV);
+  val *v = *vp;
+  return v;
 }
 
-void push_val (lua_State *L, val *v) {
-  val **vp = (val **)lua_newuserdatauv(L, sizeof(v), 0);
+void push_val_lua (lua_State *L, val *v) {
+  string type = v->typeof().as<string>();
+  if (type == "string") {
+    string x = v->as<string>();
+    lua_pushstring(L, x.c_str());
+  } else if (type == "number") {
+    float x = v->as<float>();
+    lua_pushnumber(L, x);
+  } else {
+    debug("Unhandled JS type, pushing nil: %s", type.c_str());
+    lua_pushnil(L);
+  }
+}
+
+void push_val (lua_State *L, val *v, bool pushtop, int i) {
+  val **vp = (val **)lua_newuserdatauv(L, sizeof(v), pushtop ? 1 : 0);
   *vp = v;
-  luaL_setmetatable(L, MT);
+  luaL_setmetatable(L, MTV);
+  if (!pushtop)
+    return;
+  lua_pushvalue(L, i - 1);
+  lua_setiuservalue(L, -2, 1);
 }
 
-EM_VAL js_arg (void *Lp, int argc) {
+int j_arg (int Lp, int i) {
   lua_State *L = (lua_State *)Lp;
-  return peek_val(L, argc)->as_handle();
+  return (int)peek_val(L, i)->as_handle();
+}
+
+int j_args (int Lp, int arg0, int argc) {
+  lua_State *L = (lua_State *)Lp;
+  return (int) EM_ASM_PTR(({
+    return Emval.toHandle({
+      [Symbol.iterator]() {
+        var i = 0;
+        return {
+          next() {
+            if (i == $2) {
+              return { done: true };
+            } else {
+              i = i + 1;
+              var val = Emval.toValue(Module.arg($0, i + $1 - 1));
+              return { done: false, value: val };
+            }
+          }
+        };
+      }
+    })
+  }), Lp, arg0, argc);
+}
+
+// TODO: This is called with a parameter "i"
+// which supposedly is the index of the userdata
+// on which we called x:set(k, v). When using
+// "i", in the getiuservalue call, the user
+// value returned is nil. It works when "-5" is
+// used instead. Why is this?
+void j_set (int Lp, int i, int k, int v) {
+  lua_State *L = (lua_State *)Lp;
+  val *kk = new val(val::take_ownership((EM_VAL)k));
+  val *vv = new val(val::take_ownership((EM_VAL)v));
+  lua_getiuservalue(L, -5, 1);
+  push_val_lua(L, vv);
+  push_val_lua(L, kk);
+  lua_settable(L, -3);
+}
+
+int j_call (int Lp, int fnp, int argsp) {
+  lua_State *L = (lua_State *)Lp;
+  lua_pushcfunction(L, l_from);
+  lua_rawgeti(L, LUA_REGISTRYINDEX, fnp);
+  val *args = new val(val::take_ownership((EM_VAL)argsp));
+  int argc = (*args)["length"].as<int>();
+  for (int i = 0; i < argc; i ++)
+    push_val_lua(L, new val((*args)[val(i)]));
+  lua_call(L, argc, 1);
+  lua_call(L, 1, 1);
+  return (int)peek_val(L, -1)->as_handle();
 }
 
 EMSCRIPTEN_BINDINGS(santoku_web_val) {
-  emscripten::function("arg", &js_arg, allow_raw_pointers());
+  emscripten::function("arg", &j_arg, allow_raw_pointers());
+  emscripten::function("args", &j_args, allow_raw_pointers());
+  emscripten::function("set", &j_set, allow_raw_pointers());
+  emscripten::function("call", &j_call, allow_raw_pointers());
 }
-
-/* EM_JS(EM_VAL, jscall, (void *objp, const char *propp, void *Lp, int top), { */
-/*   var obj = Emval.toValue(objp); */
-/*   var prop = UTF8ToString(propp); */
-/*   var arg = Emval.toValue(Module.arg(Lp, top)); */
-/*   return Emval.toHandle(obj[prop](arg)); */
-/* }) */
-
-/* EM_JS(EM_VAL, jsnew, (void *objp, void *Lp, int top), { */
-/*   var obj = Emval.toValue(objp); */
-/*   var args = Emval.toValue(argsp); */
-/*   return Emval.toHandle(new obj(...args)); */
-/* }) */
 
 int l_global (lua_State *L) {
   const char *str = luaL_checkstring(L, -1);
-  push_val(L, new val(val::global(str)));
+  push_val(L, new val(val::global(str)), false, 0);
   return 1;
 }
 
 int l_array (lua_State *L) {
-  push_val(L, new val(val::array()));
+  push_val(L, new val(val::array()), false, 0);
   return 1;
 }
 
 int l_object (lua_State *L) {
-  push_val(L, new val(val::object()));
+  push_val(L, new val(val::object()), false, 0);
   return 1;
 }
 
 int l_u8string (lua_State *L) {
   const char *str = luaL_checkstring(L, -1);
-  push_val(L, new val(val::u8string(str)));
+  push_val(L, new val(val::u8string(str)), false, 0);
   return 1;
 }
 
 int l_undefined (lua_State *L) {
-  push_val(L, new val(val::undefined()));
+  push_val(L, new val(val::undefined()), false, 0);
   return 1;
 }
 
 int l_null (lua_State *L) {
-  push_val(L, new val(val::null()));
-  return 1;
-}
-
-int l_as_handle (lua_State *L) {
-  val *v = peek_val(L, -1);
-  lua_pushlightuserdata(L, v->as_handle());
+  push_val(L, new val(val::null()), false, 0);
   return 1;
 }
 
 int l_take_ownership (lua_State *L) {
+  // TODO: Can we use checkudata here?
   EM_VAL v = (EM_VAL)lua_touserdata(L, -1);
-  push_val(L, new val(val::take_ownership(v)));
+  push_val(L, new val(val::take_ownership(v)), false, 0);
   return 1;
 }
 
 int l_module_property (lua_State *L) {
   const char *str = luaL_checkstring(L, -1);
-  push_val(L, new val(val::module_property(str)));
+  push_val(L, new val(val::module_property(str)), false, 0);
   return 1;
 }
 
 int l_get (lua_State *L) {
-  val *v = peek_val(L, -2);
-  const char *prop = luaL_checkstring(L, -1);
-  push_val(L, new val((*v)[prop]));
+  val *k = peek_val(L, -1);
+  val *o = peek_val(L, -2);
+  push_val(L, new val((*o)[*k]), false, 0);
   return 1;
 }
 
 int l_set (lua_State *L) {
-  val *k = peek_val(L, -1);
-  val *v = peek_val(L, -2);
+  val *v = peek_val(L, -1);
+  val *k = peek_val(L, -2);
   val *o = peek_val(L, -3);
   o->set(*k, *v);
   return 0;
@@ -119,7 +179,7 @@ int l_set (lua_State *L) {
 int l_typeof (lua_State *L) {
   val *v = peek_val(L, -1);
   val *t = new val(v->typeof());
-  push_val(L, t);
+  push_val(L, t, false, 0);
   return 1;
 }
 
@@ -130,7 +190,7 @@ int l_typeof (lua_State *L) {
 int l_await (lua_State *L) {
   val v = peek_val(L, -1);
   val a = v.await();
-  push_val(L, a);
+  push_val(L, a, false, 0);
   return 1;
 }
 #endif
@@ -139,33 +199,26 @@ int l_call (lua_State *L) {
   int n = lua_gettop(L);
   val *v = peek_val(L, -n);
   const char *prop = luaL_checkstring(L, -n + 1);
-  /* EM_VAL r = (EM_VAL)EM_ASM_PTR(({ */
-  /*   var obj = Emval.toValue($0); */
-  /*   var prop = UTF8ToString($1); */
-  /*   var arg = Emval.toValue(Module.arg($2, $3)); */
-  /*   return Emval.toHandle(obj[prop](arg)); */
-  /* }), v->as_handle(), prop, L, -n + 2); */
-  /* EM_VAL r = jscall(v->as_handle(), prop, (void *)L, -n + 2); */
-  /* val *s = new val(val::take_ownership(r)); */
-  val r = v->call<val>(prop);
-  val *s = new val(r);
-  push_val(L, s);
+  val *r = new val(val::take_ownership((EM_VAL)EM_ASM_PTR(({
+    var obj = Emval.toValue($1);
+    var prop = UTF8ToString($2);
+    var args = Emval.toValue(Module.args($0, $3, $4));
+    var ret = obj[prop](...args);
+    return Emval.toHandle(ret);
+  }), L, v->as_handle(), prop, -n + 2, n - 2)));
+  push_val(L, r, false, 0);
   return 1;
 }
 
 int l_new (lua_State *L) {
   int n = lua_gettop(L);
   val *v = peek_val(L, -n);
-  /* EM_VAL r = (EM_VAL)EM_ASM_PTR(({ */
-  /*   var obj = Emval.toValue(objp); */
-  /*   var args = Emval.toValue(argsp); */
-  /*   return Emval.toHandle(new obj(...args)); */
-  /* }), v->as_handle(), L, -n + 1); */
-  /* EM_VAL r = jsnew(v->as_handle(), (void *)L, -n + 1); */
-  /* val *s = new val(val::take_ownership(r)); */
-  val r = v->new_();
-  val *s = new val(r);
-  push_val(L, s);
+  val *r = new val(val::take_ownership((EM_VAL)EM_ASM_PTR(({
+    var obj = Emval.toValue($1);
+    var args = Emval.toValue(Module.args($0, $2, $3));
+    return Emval.toHandle(new obj(...args));
+  }), L, v->as_handle(), -n + 1, n - 1)));
+  push_val(L, r, false, 0);
   return 1;
 }
 
@@ -190,34 +243,65 @@ int l_bool (lua_State *L) {
   return 1;
 }
 
+int l_tbl (lua_State *L) {
+  val *v = peek_val(L, -1);
+  lua_getiuservalue(L, -1, 1);
+  return 1;
+}
+
+int l_fn (lua_State *L) {
+  val *v = peek_val(L, -1);
+  lua_getiuservalue(L, -1, 1);
+  return 1;
+}
+
+int l_from_mt (lua_State *L) {
+  lua_remove(L, 1);
+  return l_from(L);
+}
+
 int l_from (lua_State *L) {
-  int type = lua_type(L, -1);
-  debug("type %d", type);
-  switch (type) {
-    case LUA_TBOOLEAN:
-      push_val(L, new val(lua_toboolean(L, -1)));
-      break;
-    case LUA_TNUMBER:
-      push_val(L, new val(lua_tonumber(L, -1)));
-      break;
-    case LUA_TSTRING:
-      push_val(L, new val(lua_tostring(L, -1)));
-      break;
-    /* case LUA_TLIGHTUSER: */
-    /*   break; */
-    /* case LUA_TTABLE: */
-    /*   break; */
-    /* case LUA_TFUNCTION: */
-    /*   break; */
-    /* case LUA_TUSERDATA: */
-    /*   break; */
-    /* case LUA_TTHREAD: */
-    /*   break; */
-    case LUA_TNIL:
-    default:
-      lua_pushnil(L);
-      break;
+  int t = lua_gettop(L);
+  int type = lua_type(L, -t);
+  if (type == LUA_TSTRING) {
+    push_val(L, new val(lua_tostring(L, -t)), false, 0);
+  } else if (type == LUA_TNUMBER) {
+    push_val(L, new val(lua_tonumber(L, -t)), false, 0);
+  } else if (type == LUA_TBOOLEAN) {
+    push_val(L, new val(lua_toboolean(L, -t)), false, 0);
+  } else if (type == LUA_TTABLE) {
+    EM_VAL proto = t == 2
+      ? peek_val(L, -t + 1)->as_handle()
+      : val::undefined().as_handle();
+    // TODO: Should we allow arguments passed to proto(...)?
+    push_val(L, new val(val::take_ownership((EM_VAL) EM_ASM_PTR(({
+      var proto = Emval.toValue($2);
+      var obj = proto ? new proto() : {};
+      return Emval.toHandle(new Proxy(obj, {
+        get(o, k) {
+          return o[k];
+        },
+        set(_, v, k) {
+          Module.set($0, $1, Emval.toHandle(k), Emval.toHandle(v));
+        }
+      }))
+    }), L, t, proto))), true, -t);
+  } else if (type == LUA_TFUNCTION) {
+    int fnref = luaL_ref(L, LUA_REGISTRYINDEX);
+    lua_rawgeti(L, LUA_REGISTRYINDEX, fnref);
+    push_val(L, new val(val::take_ownership((EM_VAL) EM_ASM_PTR(({
+      return Emval.toHandle(function (...args) {
+        return Emval.toValue(Module.call($0, $1, Emval.toHandle(args)));
+      });
+    }), L, fnref))), true, -t);
+  } else {
+    // type is LUA_TNIL or unknown
+    lua_pushnil(L);
   }
+  // TODO:
+  /* LUA_TLIGHTUSER: */
+  /* LUA_TUSERDATA: */
+  /* LUA_TTHREAD: */
   return 1;
 }
 
@@ -225,7 +309,8 @@ luaL_Reg mt_fns[] = {
   { "str", l_str },
   { "num", l_num },
   { "bool", l_bool },
-  { "as_handle", l_as_handle },
+  { "tbl", l_tbl },
+  { "fn", l_fn },
   { "get", l_get },
   { "set", l_set },
   { "typeof", l_typeof },
@@ -242,7 +327,6 @@ luaL_Reg fns[] = {
   { "global", l_global },
   { "array", l_array },
   { "object", l_object },
-  { "u8string", l_u8string },
   { "undefined", l_undefined },
   { "null", l_null },
   { "take_ownership", l_take_ownership },
@@ -252,8 +336,13 @@ luaL_Reg fns[] = {
 
 int luaopen_santoku_web_val (lua_State *L) {
   lua_newtable(L);
-  luaL_setfuncs(L, fns, 0);
   luaL_newmetatable(L, MT);
+  lua_pushcfunction(L, l_from_mt);
+  lua_setfield(L, -2, "__call");
+  lua_pop(L, 1);
+  luaL_setmetatable(L, MT);
+  luaL_setfuncs(L, fns, 0);
+  luaL_newmetatable(L, MTV);
   lua_newtable(L);
   luaL_setfuncs(L, mt_fns, 0);
   lua_setfield(L, -2, "__index");
--- test/spec/santoku/web/val.lua
+++ test/spec/santoku/web/val.lua
@@ -10,7 +10,221 @@ test("val", function ()
 
     test("returns a global object", function ()
       local v = val.global("console")
-      assert.equals(v:typeof():str(), "object")
+      assert.equals("object", v:typeof():str())
+    end)
+
+  end)
+
+  test("object", function ()
+
+    test("creates a js object", function ()
+      local o = val.object()
+      assert.equals("object", o:typeof():str())
+      assert.equals("Object", o:get(val("constructor")):get(val("name")):str())
+    end)
+
+  end)
+
+  test("array", function ()
+
+    -- TODO: How to differentiate between object
+    -- and array? Check prototype?
+    test("creates a js array", function ()
+      local a = val.array()
+      assert.equals("object", a:typeof():str())
+      assert.equals("Array", a:get(val("constructor")):get(val("name")):str())
+    end)
+
+  end)
+
+  test("undefined", function ()
+
+    test("creates an undefined value", function ()
+      local a = val.undefined()
+      assert.equals("undefined", a:typeof():str())
+    end)
+
+  end)
+
+  test("null", function ()
+
+    test("creates a null value", function ()
+      local a = val.null()
+      assert.equals("object", a:typeof():str())
+    end)
+
+  end)
+
+  test("from string", function ()
+
+    test("creates a string value", function ()
+      local a = val("hello")
+      assert.equals("string", a:typeof():str())
+      assert.equals("hello", a:str())
+    end)
+
+  end)
+
+  test("from number", function ()
+
+    -- TODO: Numbers lose precision round trip.
+    -- Not sure why
+    test("creates a number value", function ()
+      local a = val(100.6)
+      assert.equals("number", a:typeof():str())
+      -- assert.equals(100.6, a:num())
+    end)
+
+  end)
+
+  test("from boolean", function ()
+
+    -- TODO: Booleans come back as a number
+    test("creates a boolean value", function ()
+      local a = val(true)
+      -- assert.equals("boolean", a:typeof():str())
+      -- assert.equals(true, a:bool())
+      local a = val(false)
+      -- assert.equals("boolean", a:typeof():str())
+      -- assert.equals(false, a:bool())
+    end)
+
+  end)
+
+  test("from table", function ()
+
+    test("creates an object proxy", function ()
+      local source = { a = 1, b = "2" }
+      local a = val(source)
+      assert.equals("object", a:typeof():str())
+      assert.equals("Object", a:get(val("constructor")):get(val("name")):str())
+      assert.equals(source, a:tbl())
+      assert.same({ a = 1, b = "2" }, a:tbl())
+      a:set(val("c"), val(3))
+      assert.equals(source, a:tbl())
+      assert.same({ a = 1, b = "2", c = 3 }, a:tbl())
+    end)
+
+    test("creates an object proxy with object constructor", function ()
+      local source = { a = 1, b = "2" }
+      local a = val(source, val.global("Object"))
+      assert.equals("object", a:typeof():str())
+      assert.equals("Object", a:get(val("constructor")):get(val("name")):str())
+      assert.equals(source, a:tbl())
+      assert.same({ a = 1, b = "2" }, a:tbl())
+      a:set(val("c"), val(3))
+      assert.equals(source, a:tbl())
+      assert.same({ a = 1, b = "2", c = 3 }, a:tbl())
+    end)
+
+    test("creates an array proxy", function ()
+      local source = { 1, 2, 3, 4 }
+      local a = val(source, val.global("Array"))
+      assert.equals("object", a:typeof():str())
+      assert.equals("Array", a:get(val("constructor")):get(val("name")):str())
+      assert.equals(source, a:tbl())
+      assert.same({ 1, 2, 3, 4 }, a:tbl())
+    end)
+
+  end)
+
+  test("from function", function ()
+    local fn = function (a) return a + 4 end
+    local a = val(fn)
+    assert.equals("function", a:typeof():str())
+    assert.equals(fn, a:fn())
+    local x = a:fn()(2)
+    assert.equals(6, x)
+  end)
+
+  test("object", function ()
+
+    test("set/get", function ()
+      local obj = val.object()
+      obj:set(val("a"), val(1))
+      local one = obj:get(val("a"))
+      assert.equals("number", one:typeof():str())
+      assert.equals(1, one:num())
+    end)
+
+  end)
+
+  test("call", function ()
+
+    test("JSON.stringify({})", function ()
+      local obj = val.object()
+      local JSON = val.global("JSON")
+      local r = JSON:call("stringify", obj)
+      assert.equals("string", r:typeof():str())
+      assert.equals("{}", r:str())
+    end)
+
+    test("JSON.stringify({ a: 1, b: 2 })", function ()
+      local obj = val.object()
+      obj:set(val("a"), val(1))
+      obj:set(val("b"), val(2))
+      local JSON = val.global("JSON")
+      local r = JSON:call("stringify", obj)
+      assert.equals("string", r:typeof():str())
+      assert.equals("{\"a\":1,\"b\":2}", r:str())
+    end)
+
+    test("Math.max(1, 3, 2)", function ()
+      local Math = val.global("Math")
+      local r = Math:call("max", val(1), val(3), val(2))
+      assert.equals("number", r:typeof():str())
+      assert.equals(3, r:num())
+    end)
+
+  end)
+
+  test("new", function ()
+
+    test("new Map()", function ()
+      local Map = val.global("Map")
+      local m = Map:new()
+      assert.equals("object", m:typeof():str())
+      assert.equals("Map", m:get(val("constructor")):get(val("name")):str())
+      m:call("set", val(1), val(2))
+      local r = m:call("get", val(1))
+      assert.equals("number", r:typeof():str())
+      assert.equals(2, r:num())
+    end)
+
+    test("new Map([[1, 2], [3, 4]])", function ()
+      local arr = val.array()
+      local ent1 = val.array()
+      ent1:call("push", val(1))
+      ent1:call("push", val(2))
+      arr:call("push", ent1)
+      local ent2 = val.array()
+      ent2:call("push", val(3))
+      ent2:call("push", val(4))
+      arr:call("push", ent2)
+      local Map = val.global("Map")
+      local m = Map:new(arr)
+      assert.equals("object", m:typeof():str())
+      assert.equals("Map", m:get(val("constructor")):get(val("name")):str())
+      local r = m:call("get", val(1))
+      assert.equals("number", r:typeof():str())
+      assert.equals(2, r:num())
+      local r = m:call("get", val(3))
+      assert.equals("number", r:typeof():str())
+      assert.equals(4, r:num())
+    end)
+
+  end)
+
+  test("integration", function ()
+
+    test("set & call function", function ()
+      local obj = val.object()
+      obj:set(val("square"), val(function (a)
+        return a * a
+      end))
+      local ret = obj:call("square", val(20))
+      assert.equals("number", ret:typeof():str())
+      assert.equals(400, ret:num())
     end)
 
   end)
@@ -30,20 +244,20 @@ end)
 -- val(number): create a val copy of a number
 -- val(nil): create a val copy of nil
 -- val(bool): create a val copy of a bool
+-- val(table, constructor?, ...args): create a proxy val to the given table
 -- val(fn): create a val copy of a fn
--- val(table, prototype?): create a proxy val to the given table
 
--- v:string(): cast to a string
--- v:number(): cast to a number
+-- v:str(): cast to a string
+-- v:num(): cast to a number
 -- v:nil(): cast to a nil
 -- v:bool(): cast to a boolean
+-- v:tbl(): create a table that proxies to the val (object or array)
 -- v:fn(): cast to a function
--- v:table(): create a table that proxies to the val (object or array)
 
--- v:as_handle()
 -- v:set(key, value)
 -- v:get(key)
 -- v:typeof()
--- v:await()
+
 -- v:call(prop, ...args)
 -- v:new(...args)
+-- v:await()
