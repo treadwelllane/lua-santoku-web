742d9361f6fc8d4d2108449efa446b230f04f9ef
Matthew Brooks <mb964033@gmail.com>
2023-05-22 00:26:32 +0000

    misc

--- test/spec/santoku/web/js.lua
+++ test/spec/santoku/web/js.lua
@@ -1,9 +1,13 @@
--- TODO: high-level javascript DSL
-
 local assert = require("luassert")
 local test = require("santoku.test")
 local js = require("santoku.web.js")
 
 test("js", function ()
+
+  local console = js.console
+
+  -- js:setTimeout(function ()
+  --   console:log("It works")
+  -- end)
 
 end)
