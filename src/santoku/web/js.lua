2bf0c152ba0ecc00100cd737ad5c9334fef083bf
Matthew Brooks <mb964033@gmail.com>
2023-05-22 00:20:41 +0000

    most tests working

--- src/santoku/web/js.lua
+++ src/santoku/web/js.lua
@@ -1,31 +1,7 @@
 local val = require("santoku.web.val")
-local tup = require("santoku.tuple")
-
-local function wrap (v, parent, prop)
-  local t = v:typeof():str()
-  if t == "string" then
-    return v:str()
-  elseif t == "number" then
-    return v:num()
-  elseif t == "boolean" then
-    return v:bool()
-  elseif t == "undefined" or t == "null" then
-    return nil
-  elseif t == "function" then
-    return function (...)
-      return wrap(parent:call(prop, tup.map(val.from, ...)))
-    end
-  else
-    return setmetatable({}, {
-      __index = function (_, k)
-        return wrap(v:get(k), v, k)
-      end
-    })
-  end
-end
 
 return setmetatable({}, {
   __index = function (_, k)
-    return wrap(val.global(k))
+    return val.global(k):lua()
   end
 })
--- test/spec/santoku/web/js.lua
+++ test/spec/santoku/web/js.lua
@@ -4,15 +4,6 @@ local assert = require("luassert")
 local test = require("santoku.test")
 local js = require("santoku.web.js")
 
--- val = require("santoku.web.val")
--- js = require("santoku.web.js")
--- js[x]: val.global(...)
+test("js", function ()
 
--- console = js.console
--- console.log(1, 2, 3, { 1, 2, 3 }): auto proxied/copied arguments
--- console.log(1, 2, 3, val({ 1, 2, 3 }, js.Array)): specify prototype
-
--- Map = js.Map
--- m = Map:new()
--- m:set(1, 2)
--- m:get(1)
+end)
