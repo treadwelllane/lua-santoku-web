3734ab57333d5985d780a9d7d46d938a39775a13
broma0 <121064585+broma0@users.noreply.github.com>
2023-06-24 00:59:32 +0000

    simplify worker rpc array construction

--- src/santoku/web/worker/rpc/server.lua
+++ src/santoku/web/worker/rpc/server.lua
@@ -1,6 +1,5 @@
 local js = require("santoku.web.js")
 local val = require("santoku.web.val")
-local tup = require("santoku.tuple")
 local compat = require("santoku.compat")
 
 local global = js.self
@@ -12,16 +11,7 @@ M.init = function (obj)
     local ch = ev.ports[0]
     local fn = ev.data[0]
     local args = ev.data:slice(1)
-    local out = val.array()
-    local ret = tup(obj[fn](compat.unpack(args)))
-    -- TODO: santoku web should expose helper
-    -- function to convert to js, or at least
-    -- allow structured clones of proxy objects
-    for i = 1, tup.len(ret()) do
-      out:set(i - 1, tup.get(i, ret()))
-    end
-    -- TODO: Need to serialize results from obj
-    -- call for postMessage
+    local out = val.array(obj[fn](compat.unpack(args)))
     ch:postMessage(out)
   end)
 end
