8ca5a0713874622b5c5fa7034cd608085fd95fdc
broma0 <121064585+broma0@users.noreply.github.com>
2023-08-03 11:58:55 +0000

    implement lua conversion and object support

--- test/spec/santoku/web/js.lua
+++ test/spec/santoku/web/js.lua
@@ -2,47 +2,9 @@ local assert = require("luassert")
 local test = require("santoku.test")
 local compat = require("santoku.compat")
 local gen = require("santoku.gen")
+local vec = require("santoku.vector")
 local val = require("santoku.web.val")
 local js = require("santoku.web.js")
-
--- TODO
---
--- val(x [, false]): converts lua value 'x' to a
--- javascript value, returning a val userdata.
--- If 'x' is a table, the val userdata returned
--- is a javascript Proxy object which wraps the
--- lua table.
---
--- val(x, true): similar to val(x [, false]),
--- except that if 'x' is a table, the table and
--- its keys and values are traversed and
--- converted to javascript primitives, objects,
--- and arrays by recursively calling this
--- function. Recursion stops whenever a
--- javascript value that is not a lua Proxy is
--- encountered.
---
--- x:val([, false]): called on an existing val,
--- promise, object, etc., returning it as a val.
---
--- x:val(true): called on an existing val. If
--- the val is a proxy to a lua table, behaves
--- like val(x, true), otherwise returns the val
--- as-is.
---
--- x:lua([, false]): converts a javascript value
--- to a lua value, and either returns the lua
--- primitive or a proxy-table that wraps the
--- underlying javascript object.
---
--- x:lua(true): similar to x:lua([, false]),
--- except that if the javascript value is an
--- object, the object and its keys and values
--- are traversed and converted to lua primitives
--- and tables by recursively calling this
--- function. Recursion stops whenever a lua
--- value that is not a javascript proxy is
--- encountered.
 
 test("js", function ()
 
@@ -115,5 +77,29 @@ test("js", function ()
     local obj = val({ a = 1 }, true):lua()
     assert.same({{"a", 1, n = 2}, n = 1}, gen.pairs(obj):vec())
   end)
+
+  test("Object.keys() on wrapped val", function ()
+    local obj = val({ a = 1, b = 2 })
+    local keys = js.Object:keys(obj)
+    local vkeys = vec(compat.unpack(keys)):sort()
+    assert.same({ "a", "b", n = 2 }, vkeys)
+  end)
+
+  test("Object.values() on wrapped val", function ()
+    local obj = val({ a = 1, b = 2 })
+    local values = js.Object:values(obj)
+    local vvalues = vec(compat.unpack(values)):sort()
+    assert.same({ 1, 2, n = 2 }, vvalues)
+  end)
+
+  -- TODO: This depends on x:lua(true) working,
+  -- which converts a wrapped JS object to a lua
+  -- table
+  --
+  -- test("Object.entries() on wrapped val", function ()
+  --   local obj = val({ a = 1, b = 2 })
+  --   local entries = js.Object:entries(obj)
+  --   print(entries:lua(true))
+  -- end)
 
 end)
