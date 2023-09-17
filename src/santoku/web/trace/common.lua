c58aea78693efb57c9ee7271027c89c6c2f89645
Matthew Brooks <mb964033@gmail.com>
2023-09-17 14:20:25 +0000

    do less in trace

--- src/santoku/web/trace/common.lua
+++ src/santoku/web/trace/common.lua
@@ -1,9 +1,3 @@
--- TODO: Lines corresponding to wrapFetch are
--- commented. See the TODO below for details.
---
--- local gen = require("santoku.gen")
--- local tup = require("santoku.tuple")
-
 local vec = require("santoku.vector")
 
 return function (callback, global, opts)
@@ -12,10 +6,7 @@ return function (callback, global, opts)
 
   local JSON = global.JSON
   local console = global.console
-  -- local Request = global.Request
-  -- local Promise = global.Promise
 
-  local oldwinerr = global and global.onerror
   local oldfetch = global and global.fetch
   local logtypes = vec("log", "error")
   local oldlogs = {}
@@ -52,97 +43,17 @@ return function (callback, global, opts)
     end)
   end
 
-  local function onErr (ev)
-    callback(JSON:stringify({
-      source = "error",
-      event = ev,
-      name = ev and ev.name,
-      message = ev and ev.message,
-    }))
-    if oldwinerr then
-      oldwinerr(console, ev)
-    end
-  end
-
-  local function wrapErr ()
-    if global then
-      global:addEventListener("error", function (_, ev)
-        onErr(ev)
-      end)
-    end
-  end
-
-  -- TODO: This is causing failures loading
-  -- sqlite and eruda
-  --
-  -- local function wrapFetch ()
-  --   if global and oldfetch then
-  --     global.fetch = function (thisfetch, r, ...)
-  --       local args = tup(...)
-  --       local req
-  --       if type(r) == "string" or not r:instanceof(Request) then
-  --         req = Request:new(r, args())
-  --       else
-  --         req = r:clone()
-  --       end
-  --       return Promise:new(function (thisp, resolve)
-  --         req:text():await(function (_, ok, body)
-  --           assert(ok)
-  --           callback(JSON:stringify({
-  --             source = "fetch",
-  --             request = {
-  --               url = req.url,
-  --               method = req.method,
-  --               body = body,
-  --               headers = { gen.ivals(req.headers):unpack() }
-  --             }
-  --           }))
-  --           oldfetch(thisfetch, r, args()):await(function(_, ok, resp)
-  --             assert(ok)
-  --             local clone = resp:clone()
-  --             return clone:text():await(function (_, ok, body)
-  --               assert(ok)
-  --               callback(JSON:stringify({
-  --                 source = "fetch",
-  --                 response = {
-  --                   url = clone.url,
-  --                   method = clone.method,
-  --                   status = clone.status,
-  --                   body = body,
-  --                   headers = { gen.ivals(clone.headers):unpack() },
-  --                 }
-  --               }))
-  --               resolve(thisp, resp)
-  --             end)
-  --           end)
-  --         end)
-  --       end)
-  --     end
-  --   end
-  -- end
-
   if opts.print ~= false then
     wrapPrint()
-  end
-
-  if opts.error ~= false then
-    wrapErr()
   end
 
   if opts.logs ~= false then
     wrapLogs()
   end
 
-  -- TODO: see above
-  -- if opts.fetch ~= false then
-  --   wrapFetch()
-  -- end
-
   return {
     oldlogs = oldlogs,
-    oldwinerr = oldwinerr,
     oldfetch = oldfetch,
-    onErr = onErr
   }
 
 end
--- src/santoku/web/trace/sw.lua
+++ src/santoku/web/trace/sw.lua
@@ -3,7 +3,6 @@ local js = require("santoku.web.js")
 
 local global = js.self
 local BroadcastChannel = global.BroadcastChannel
-local JSON = global.JSON
 
 local channel = BroadcastChannel:new("santoku.web.trace")
 
@@ -16,17 +15,5 @@ return function (opts)
   end
 
   common(emit, global, opts)
-
-  if opts.fetch ~= false then
-    global:addEventListener("fetch", function (_, ev)
-      emit(JSON:stringify({
-        source = "fetch-event",
-        request = {
-          url = ev.request.url,
-          method = ev.request.method
-        }
-      }))
-    end)
-  end
 
 end
