e2463b9fcf585a6b275409294787f9ae84c1eb9a
Matthew Brooks <mb964033@gmail.com>
2023-09-26 13:39:51 +0000

    add exceptions to trace

--- src/santoku/web/trace/common.lua
+++ src/santoku/web/trace/common.lua
@@ -36,12 +36,38 @@ return function (callback, global, opts)
     end)
   end
 
+  local function wrapError ()
+    if global.addEventListener then
+      global:addEventListener("error", function (_, ev)
+        callback(JSON:stringify({ "error", ev and ev.message or ev }))
+      end)
+      global:addEventListener("uncaughtException", function (_, ev)
+        callback(JSON:stringify({ "uncaughtException", ev and ev.message or ev }))
+      end)
+      global:addEventListener("unhandledRejection", function (_, ev)
+        callback(JSON:stringify({ "unhandledRejection", ev and ev.message or ev }))
+      end)
+    end
+    if global.process and global.process.on then
+      global.process:on("uncaughtException", function (_, ev)
+        callback(JSON:stringify({ "uncaughtException", ev and ev.message or ev }))
+      end)
+      global.process:on("unhandledRejection", function (_, ev)
+        callback(JSON:stringify({ "unhandledRejection", ev and ev.message or ev }))
+      end)
+    end
+  end
+
   if opts.print ~= false then
     wrapPrint()
   end
 
   if opts.logs ~= false then
     wrapLogs()
+  end
+
+  if opts.error ~= false then
+    wrapError()
   end
 
   return {
