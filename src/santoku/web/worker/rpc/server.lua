83d22143b7f43299e53c8b63a155133ba910e9ea
broma0 <121064585+broma0@users.noreply.github.com>
2023-10-04 11:00:41 +0000

    refactor rpc server for async support

--- src/santoku/web/worker/rpc/server.lua
+++ src/santoku/web/worker/rpc/server.lua
@@ -4,12 +4,20 @@ local compat = require("santoku.compat")
 local M = {}
 
 M.init = function (obj, on_message)
-  on_message(function (ev)
+  return on_message(function (ev)
     local ch = ev.ports[1]
-    local fn = ev.data[1]
+    local key = ev.data[1]
     local args = ev.data:slice(1)
-    local out = val({ obj[fn](compat.unpack(args)) }, true)
-    ch:postMessage(out)
+    if obj.async and obj.async[key] then
+      args:push(function (...)
+        return ch:postMessage(val({ ... }, true))
+      end)
+      return obj.async[key](compat.unpack(args))
+    elseif obj[key] then
+      return ch:postMessage(val({ obj[key](compat.unpack(args)) }, true))
+    else
+      return ch:postMessage(val({ false, "Property '" .. tostring(key) .. "' not found" }, true))
+    end
   end)
 end
 
