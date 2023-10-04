79e202e0f863cf620818cd44dff0f85394a0ffe8
Matthew Brooks <mb964033@gmail.com>
2023-10-04 02:08:08 +0000

    auto transfer transferables in wrpc

--- src/santoku/web/worker/rpc/client.lua
+++ src/santoku/web/worker/rpc/client.lua
@@ -1,6 +1,7 @@
 local js = require("santoku.web.js")
 local val = require("santoku.web.val")
 local err = require("santoku.err")
+local gen = require("santoku.gen")
 local tup = require("santoku.tuple")
 local compat = require("santoku.compat")
 
@@ -9,27 +10,60 @@ local MessageChannel = js.MessageChannel
 
 local M = {}
 
+local transferables = gen.pack(
+  "ArrayBuffer",
+  "MessagePort",
+  "ReadableStream",
+  "WritableStream",
+  "TransformStream",
+  "WebTransportReceiveStream",
+  "AudioData",
+  "ImageBitmap",
+  "VideoFrame",
+  "OffscreenCanvas",
+  "RTCDataChannel"
+):set()
+
+-- TODO: currently this only supports
+-- transferables that occur at the top-level of
+-- arguments, not nested within objects and
+-- tables. Ideally, the val(..., true)
+-- traversal could optionally return a table of
+-- all transferables encountered.
 M.init = function (fp)
   return err.pwrap(function (check)
+
     local worker = check(pcall(Worker.new, Worker, fp))
+
     return setmetatable({}, {
       __index = function (_, k)
         return function (...)
+
           local mc = MessageChannel:new()
           local n = tup.len(...)
-          -- TODO: Consider await or similar
-          -- structure instead of expecting a
-          -- callback as the last function
-          -- argument, which might be confusing.
           local callback = tup.get(n, ...)
-          local args = val({ k, tup.take(n - 1, ...) }, true)
-          worker:postMessage(args, { mc.port2 })
+
+          local args = tup(tup.take(n - 1, ...))
+
+          local tfrs = tup(tup.filter(function (t)
+            local ok, n = pcall(function ()
+              return t.constructor.name
+            end)
+            return ok and transferables[n]
+          end, args()))
+
+          worker:postMessage(
+            val({ k, args() }, true),
+            { mc.port2, tfrs() })
+
           mc.port1.onmessage = function (_, ev)
             callback(compat.unpack(ev.data))
           end
+
         end
       end
     }), worker
+
   end)
 end
 
