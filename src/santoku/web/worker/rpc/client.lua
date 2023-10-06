706658be51962059a8766a48ab160a0369c6fba2
broma0 <121064585+broma0@users.noreply.github.com>
2023-10-06 12:25:18 +0000

    refactor rpc to use message channels

--- src/santoku/web/worker/rpc/client.lua
+++ src/santoku/web/worker/rpc/client.lua
@@ -24,47 +24,59 @@ local transferables = gen.pack(
   "RTCDataChannel"
 ):set()
 
--- TODO: currently this only supports
--- transferables that occur at the top-level of
--- arguments, not nested within objects and
--- tables. Ideally, the val(..., true)
--- traversal could optionally return a table of
--- all transferables encountered.
 M.init = function (fp)
   return err.pwrap(function (check)
-
     local worker = check(pcall(Worker.new, Worker, fp))
-
-    return setmetatable({}, {
-      __index = function (_, k)
-        return function (...)
-
-          local mc = MessageChannel:new()
-          local n = tup.len(...)
-          local callback = tup.get(n, ...)
-
-          local args = tup(tup.take(n - 1, ...))
-
-          local tfrs = tup(tup.filter(function (t)
-            local ok, n = pcall(function ()
-              return t.constructor.name
-            end)
-            return ok and transferables[n]
-          end, args()))
-
-          worker:postMessage(
-            val({ k, args() }, true),
-            { mc.port2, tfrs() })
-
-          mc.port1.onmessage = function (_, ev)
-            callback(compat.unpack(ev.data))
-          end
-
-        end
-      end
-    }), worker
-
+    local port = M.create_port(worker)
+    return M.init_port(port), worker
   end)
+end
+
+M.create_port = function (worker)
+  local ch = MessageChannel:new()
+  M.register_port(worker, ch.port2)
+  return ch.port1
+end
+
+M.register_port = function (worker, port)
+  worker:postMessage(val({ REGISTER_PORT = port }, true), { port })
+end
+
+M.init_port = function (port)
+  return setmetatable({}, {
+    __index = function (_, k)
+      return function (...)
+
+        local ch = MessageChannel:new()
+        local n = tup.len(...)
+        local callback = tup.get(n, ...)
+
+        local args = tup(tup.take(n - 1, ...))
+
+        local tfrs = tup(tup.filter(function (t)
+          local ok, n = pcall(function ()
+            return t.constructor.name
+          end)
+          return ok and transferables[n]
+        end, args()))
+
+        -- TODO: currently this only supports
+        -- transferables that occur at the top-level of
+        -- arguments, not nested within objects and
+        -- tables. Ideally, the val(..., true)
+        -- traversal could optionally return a table of
+        -- all transferables encountered.
+        port:postMessage(
+          val({ k, ch.port2, args() }, true),
+          { ch.port2, tfrs() })
+
+        ch.port1.onmessage = function (_, ev)
+          callback(compat.unpack(ev.data))
+        end
+
+      end
+    end
+  })
 end
 
 return M
--- src/santoku/web/worker/rpc/server.lua
+++ src/santoku/web/worker/rpc/server.lua
@@ -5,18 +5,18 @@ local M = {}
 
 M.init = function (obj, on_message)
   return on_message(function (ev)
-    local ch = ev.ports[1]
+    local port = ev.data[2]
     local key = ev.data[1]
-    local args = ev.data:slice(1)
+    local args = ev.data:slice(2)
     if obj.async and obj.async[key] then
       args:push(function (...)
-        return ch:postMessage(val({ ... }, true))
+        return port:postMessage(val({ ... }, true))
       end)
       return obj.async[key](compat.unpack(args))
     elseif obj[key] then
-      return ch:postMessage(val({ obj[key](compat.unpack(args)) }, true))
+      return port:postMessage(val({ obj[key](compat.unpack(args)) }, true))
     else
-      return ch:postMessage(val({ false, "Property '" .. tostring(key) .. "' not found" }, true))
+      return port:postMessage(val({ false, "Property '" .. tostring(key) .. "' not found" }, true))
     end
   end)
 end
