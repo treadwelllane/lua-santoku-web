3553dbf6cc98a7b9d55c8935d5db774329eb35d3
broma0 <121064585+broma0@users.noreply.github.com>
2024-04-21 01:50:16 +0000

    add spa framework and associated modules

---
+++ lib/santoku/web/spa/sw.lua
@@ -0,0 +1,116 @@
+local js = require("santoku.web.js")
+local fun = require("santoku.functional")
+local util = require("santoku.web.util")
+local async = require("santoku.async")
+local it = require("santoku.iter")
+local fun = require("santoku.functional")
+local global = js.self
+local Module = global.Module
+local caches = js.caches
+local clients = js.clients
+local Promise = js.Promise
+
+local function run (opts)
+
+  opts = opts or {}
+
+  opts.cached_files = opts.cached_files ~= false and
+    opts.cached_files or
+      arr.push(it.collect(it.flatten(it.map(function (fp)
+        if not str.match(fp, "sw.js$") then
+          return it.ivals({ fp, fs.stripextensions(fp) })
+        end
+      end, it.ivals(opts.public_files or {})))), "/")
+
+  Module.on_install = function ()
+    print("Installing service worker")
+    return util.promise(function (complete)
+      return async.pipe(function (done)
+        return caches:open(version):await(fun.sel(done, 2))
+      end, function (done, cache)
+        return async.each(it.ivals(opts.cached_files), function (each_done, file)
+          return async.pipe(function (done)
+            return util.fetch(file, {
+              -- TODO: This is not currently implemented
+              retry_times = opts.cache_fetch_retry_times
+              backoff_ms = opts.cache_fetch_retry_backoff_ms
+              backoff_multiply = opts.cache_fetch_retry_backoff_multiply
+            }):await(fun.sel(done, 2))
+          end, function (done, res)
+            return cache:put(file, res):await(fun.sel(done, 2))
+          end, function (ok, err, ...)
+            if not ok then
+              print("Failed caching", file, err and err.message)
+            end
+            return each_done(ok, ...)
+          end)
+        end, done)
+      end, function (done)
+        return global:skipWaiting():await(fun.sel(done, 2))
+      end, function (ok, ...)
+        if ok then
+          print("Installed service worker")
+        else
+          print("Error installing service worker", (...) and (...).message or (...))
+        end
+        return complete(ok, ...)
+      end)
+    end)
+  end
+
+  Module.on_activate = function ()
+    print("Activating service worker")
+    return util.promise(function (complete)
+      return async.pipe(function (done)
+        return caches:keys():await(fun.sel(done, 2))
+      end, function (done, keys)
+        return Promise:all(keys:filter(function (_, k)
+          return k ~= version
+        end):map(function (_, k)
+          return caches:delete(k)
+        end)):await(fun.sel(done, 2))
+      end, function (done)
+        return clients:claim():await(fun.sel(done, 2))
+      end, function (ok, ...)
+        if ok then
+          print("Activated service worker")
+        else
+          print("Error activating service worker")
+        end
+        return complete(ok, ...)
+      end)
+    end)
+  end
+
+  Module.on_fetch = function (_, request)
+    return util.promise(function (complete)
+      return async.pipe(function (done)
+        return caches:open(version):await(fun.sel(done, 2))
+      end, function (done, cache)
+        return cache:match(request, {
+          ignoreSearch = true,
+          ignoreVary = true,
+          ignoreMethod = true
+        }):await(fun.sel(done, 2))
+      end, function (done, resp)
+        if not resp then
+          print("Cache miss", request.url)
+          return util.fetch(request):await(fun.sel(done, 2))
+        else
+          return done(true, resp:clone())
+        end
+      end, complete)
+    end)
+  end
+
+  Module:start()
+
+end
+
+return function (opts)
+  if opts.trace then
+    return require("santoku.web.trace.sw")({ name = "sw" }, run)
+  else
+    return run(opts)
+  end
+end
---
+++ lib/santoku/web/spa/wrap_events.lua
@@ -0,0 +1,9 @@
+local template = require("santoku.template")
+local inherit = require("santoku.inherit")
+local defaults = require("santoku.web.spa.defaults")
+local html = <%
+  local serialize = require("santoku.serialize")
+  return serialize(readfile("res/wrap_events.js"))
+%>
+
+return template.compile(html, nil, nil, inherit.pushindex({ opts = defaults }, _G))
