73d07eafb40210be13ff370db853f388d3e0563e
Matthew Brooks <mb964033@gmail.com>
2025-11-26 21:26:05 -0500

    update

---
+++ lib/santoku/web/pwa/index.tk.lua
@@ -0,0 +1,24 @@
+<%
+  str = require("santoku.string")
+  squote = str.quote
+  to_base64 = str.to_base64
+  fs = require("santoku.fs")
+  readfile = fs.readfile
+%>
+
+local mustache = require("santoku.mustache")
+local str = require("santoku.string")
+local tbl = require("santoku.table")
+
+local template = str.from_base64(<% return squote(to_base64(readfile("res/pwa/index.mustache"))) %>) -- luacheck: ignore
+
+local defaults = {
+  charset = "utf-8",
+  lang = "en",
+  manifest = "/manifest.json",
+  theme_color = "#000000",
+}
+
+return function(opts)
+  return mustache(template, tbl.merge({}, defaults, opts or {}))
+end
