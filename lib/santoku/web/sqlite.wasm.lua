4de0ed914bb54d9d84723a813cc69f8d31cd4d9d
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-02 14:59:56 -0500

    add sqlite opfs and sahpool support

--- lib/santoku/web/sqlite.wasm.lua
+++ lib/santoku/web/sqlite.wasm.lua
@@ -16,135 +16,158 @@ local function cast_param (p)
   end
 end
 
-M.open_opfs = function (dbfile, callback)
+local function create_db_wrapper (wsqlite, raw_db)
+  return sqlite({
+
+    db = raw_db,
+
+    exec = function (db, sql)
+      local ok, e = err.pcall(db.db.exec, db.db, sql)
+      if not ok then
+        db.err = e
+        return ERROR
+      else
+        db.err = nil
+        return OK
+      end
+    end,
+
+    prepare = function (db, sql)
+      local ok, stmt = err.pcall(db.db.prepare, db.db, sql)
+      if not ok then
+        db.err = stmt
+        return nil
+      else
+        db.err = nil
+        return setmetatable({
+
+          bind_names = function (_, t)
+            local ok0, e = err.pcall(function ()
+              Object:keys(t):forEach(function (_, k)
+                local v = t[k]
+                stmt:bind(":" .. k, cast_param(v))
+              end)
+            end)
+            if ok0 then
+              return OK
+            else
+              err.error(e)
+              return ERROR
+            end
+          end,
+
+          bind_values = function (_, ...)
+            local ok0, e = err.pcall(function (...)
+              for i = 1, select("#", ...) do
+                stmt:bind(i, cast_param(select(i, ...)))
+              end
+            end, ...)
+            if ok0 then
+              return OK
+            else
+              err.error(e)
+              return ERROR
+            end
+          end,
+
+          step = function ()
+            local ok0, res = err.pcall(stmt.step, stmt)
+            if not ok0 then
+              db.err = res
+              return ERROR
+            elseif res then
+              db.err = nil
+              return ROW
+            else
+              db.err = nil
+              return DONE
+            end
+          end,
+
+          get_named_values = function ()
+            local ret = {}
+            for i = 0, stmt.columnCount - 1 do
+              local k = val.lua(stmt:getColumnName(i))
+              local v = val.lua(stmt:get(i))
+              ret[k] = v
+            end
+            return ret
+          end,
+
+          reset = function ()
+            stmt:reset(true)
+            return OK
+          end,
+
+          columns = function ()
+            return stmt.columnCount
+          end,
+
+          get_value = function (_, n)
+            return val.lua(stmt:get(n))
+          end,
+
+        }, {
+          __index = stmt
+        })
+      end
+    end,
+
+    last_insert_rowid = function (db)
+      return wsqlite.capi:sqlite3_last_insert_rowid(db.db.pointer)
+    end,
+
+    errcode = function (db)
+      if db.err then
+        return db.err.name
+      end
+    end,
+
+    errmsg = function (db)
+      if db.err then
+        return db.err.message
+      end
+    end,
+
+  })
+end
+
+M.open_sahpool = function (dbfile, opts, callback)
+  if type(opts) == "function" then
+    callback = opts
+    opts = {}
+  end
+  opts = opts or {}
 
   js:sqlite3InitModule():await(function (_, ok, wsqlite)
-
     if not ok then
-      callback(false, wsqlite)
-      return
+      return callback(false, wsqlite)
+    end
+
+    wsqlite:installOpfsSAHPoolVfs(opts):await(function (_, ok2, pool_util)
+      if not ok2 then
+        return callback(false, pool_util)
+      end
+
+      callback(err.pcall(function ()
+        local raw_db = pool_util.OpfsSAHPoolDb:new(dbfile)
+        return create_db_wrapper(wsqlite, raw_db)
+      end))
+    end)
+  end)
+end
+
+M.open_opfs = function (dbfile, callback)
+  js:sqlite3InitModule():await(function (_, ok, wsqlite)
+    if not ok then
+      return callback(false, wsqlite)
     end
 
     callback(err.pcall(function ()
-
-      return sqlite({
-
-        db = wsqlite.oo1.OpfsDb:new(dbfile),
-
-        exec = function (db, sql)
-          local ok, e = err.pcall(db.db.exec, db.db, sql)
-          if not ok then
-            db.err = e
-            return ERROR
-          else
-            db.err = nil
-            return OK
-          end
-        end,
-
-        prepare = function (db, sql)
-          local ok, stmt = err.pcall(db.db.prepare, db.db, sql)
-          if not ok then
-            db.err = stmt
-            return nil
-          else
-            db.err = nil
-            return setmetatable({
-
-              bind_names = function (_, t)
-                local ok, e = err.pcall(function ()
-                  Object:keys(t):forEach(function (_, k)
-                    local v = t[k]
-                    stmt:bind(":" .. k, cast_param(v))
-                  end)
-                end)
-                if ok then
-                  return OK
-                else
-                  err.error(e)
-                  return ERROR
-                end
-              end,
-
-              bind_values = function (_, ...)
-                local ok, e = err.pcall(function (...)
-                  for i = 1, select("#", ...) do
-                    stmt:bind(i, cast_param(select(i, ...)))
-                  end
-                end, ...)
-                if ok then
-                  return OK
-                else
-                  err.error(e)
-                  return ERROR
-                end
-              end,
-
-              step = function ()
-                local ok, res = err.pcall(stmt.step, stmt)
-                if not ok then
-                  db.err = res
-                  return ERROR
-                elseif res then
-                  db.err = nil
-                  return ROW
-                else
-                  db.err = nil
-                  return DONE
-                end
-              end,
-
-              get_named_values = function ()
-                local ret = {}
-                for i = 0, stmt.columnCount - 1 do
-                  local k = val.lua(stmt:getColumnName(i))
-                  local v = val.lua(stmt:get(i))
-                  ret[k] = v
-                end
-                return ret
-              end,
-
-              reset = function ()
-                stmt:reset(true)
-                return OK
-              end,
-
-              columns = function ()
-                return stmt.columnCount
-              end,
-
-              get_value = function (_, n)
-                return val.lua(stmt:get(n))
-              end,
-
-            }, {
-              __index = stmt
-            })
-          end
-        end,
-
-        last_insert_rowid = function (db)
-          return wsqlite.capi:sqlite3_last_insert_rowid(db.db.pointer)
-        end,
-
-        errcode = function (db)
-          if db.err then
-            return db.err.name
-          end
-        end,
-
-        errmsg = function (db)
-          if db.err then
-            return db.err.message
-          end
-        end,
-
-      })
-
+      local raw_db = wsqlite.oo1.OpfsDb:new(dbfile)
+      return create_db_wrapper(wsqlite, raw_db)
     end))
-
   end)
-
 end
 
 return M
