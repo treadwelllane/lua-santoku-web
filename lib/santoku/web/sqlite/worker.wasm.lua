75151369d030f2423876f2956a58accb795cb2aa
Matthew Brooks <mb964033@gmail.com>
2025-12-02 16:02:35 -0500

    update

--- lib/santoku/web/sqlite/worker.wasm.lua
+++ lib/santoku/web/sqlite/worker.wasm.lua
@@ -5,25 +5,13 @@ local wrpc = require("santoku.web.worker.rpc.server")
 local global = js.self
 local Module = global.Module
 
-local function create_worker (db_path, opts, handler)
+return function (db_path, opts, handler)
   if type(opts) == "function" then
     handler = opts
-    opts = {}
-  end
-  opts = opts or {}
-
-  local open_fn = opts.sahpool and sqlite.open_sahpool or sqlite.open_opfs
-  local open_opts = opts.sahpool and opts.sahpool_opts or nil
-
-  local function do_open (callback)
-    if open_opts then
-      return open_fn(db_path, open_opts, callback)
-    else
-      return open_fn(db_path, callback)
-    end
+    opts = nil
   end
 
-  return do_open(function (ok, db)
+  return sqlite.open(db_path, opts, function (ok, db)
     return handler(ok, db, function (ok2, handlers)
       if not ok2 then
         return
@@ -41,5 +29,3 @@ local function create_worker (db_path, opts, handler)
     end)
   end)
 end
-
-return create_worker
