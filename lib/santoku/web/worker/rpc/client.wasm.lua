e568112a4f3a0b240d2d4499af9c9fa66ff8ad87
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-02 19:28:12 -0500

    restore debug logging in sqlite init

--- lib/santoku/web/worker/rpc/client.wasm.lua
+++ lib/santoku/web/worker/rpc/client.wasm.lua
@@ -24,7 +24,6 @@ local transferables = {
 }
 
 M.init = function (fp)
-  print("wrpc.init spawning worker:", fp)
   local worker = Worker:new(fp)
   local port = M.create_port(worker)
   return M.init_port(port), worker
