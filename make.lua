e22cd0192fbd6ea86dee54f8a69f99b6baecdc57
Matthew Brooks <mb964033@gmail.com>
2025-12-07 12:28:03 -0500

    update

--- make.lua
+++ make.lua
@@ -1,16 +1,16 @@
 local env = {
 
   name = "santoku-web",
-  version = "0.0.386-1",
+  version = "0.0.389-1",
   variable_prefix = "TK_WEB",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.304-1",
+    "santoku >= 0.0.305-1",
     "santoku-mustache >= 0.0.13-1",
-    "santoku-http >= 0.0.17-1",
+    "santoku-http >= 0.0.18-1",
     "santoku-sqlite >= 0.0.27-1",
     "lua-cjson == 2.1.0.10-1"
   },
