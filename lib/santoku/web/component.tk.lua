e96aab79ddd5620740b805ace49dcf95106204e6
Matthew Brooks <mb964033@gmail.com>
2026-03-30 16:46:56 -0400

    update

--- lib/santoku/web/component.tk.lua
+++ lib/santoku/web/component.tk.lua
@@ -8,17 +8,26 @@ local function replace(s, old, new)
   return s:sub(1, i - 1) .. (new or "") .. s:sub(j + 1)
 end
 
-return function (tag, html)
+return function (tag, html, opts)
   local parts = lp.component_parts(html)
+  local style = parts.style
+  local body = parts.body
+  local init = parts.init
+  if opts then
+    if opts.css then style = opts.css(style) end
+    if opts.html then body = opts.html(body) end
+    if opts.js then init = opts.js(init) end
+  end
   local deps_parts = {}
   for i = 1, #parts.deps do
     deps_parts[i] = "\"" .. parts.deps[i]:gsub("\\", "\\\\"):gsub("\"", "\\\"") .. "\""
   end
   local out = skeleton
   out = replace(out, "%TAG%", tag)
-  out = replace(out, "%STYLE%", parts.style)
-  out = replace(out, "%BODY%", parts.body)
-  out = replace(out, "%INIT%", parts.init)
+  out = replace(out, "%STYLE%", style)
+  out = replace(out, "%BODY%", body)
+  out = replace(out, "%INIT%", init)
   out = replace(out, "%DEPS%", table.concat(deps_parts, ","))
+  if opts and opts.js then out = opts.js(out) end
   return out
 end
